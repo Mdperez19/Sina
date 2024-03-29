ed7b85eefdc1542abcfd3a7f04fbf017afbb3eca
Aliss55 <alicelh1600@outlook.com>
2024-03-28 21:30:32 -0600

    added unit test for PossibleCorrection

---
+++ tests/response_entities/test_PossibleCorrections.py
@@ -0,0 +1,28 @@
+import pytest
+
+from spellchecker.response_entities.PossibleCorrections import PossibleCorrections
+
+
+class TestPossibleCorrections:
+
+    @pytest.fixture
+    def possible_corrections(self):
+        return PossibleCorrections("pricesa",
+                                   [
+                                       {'word': 'princesa', 'distance': 0.9},
+                                       {'word': 'princesita', 'distance': 0.5}
+                                   ])
+
+    @pytest.mark.PossibleCorrections
+    def test_to_dict(self, possible_corrections):
+        # Given init data
+        # When
+        result = possible_corrections.to_dict()
+        # Then
+        assert isinstance(result, dict)
+        assert result["token"] == "pricesa"
+        assert result["corrections"] == [
+                                       {'word': 'princesa', 'distance': 0.9},
+                                       {'word': 'princesita', 'distance': 0.5}
+                                   ]
+        assert isinstance(result["corrections"], list)
