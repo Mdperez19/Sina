a9bd8aaff98da0b566462157a8b3d2838b9078c1
Aliss55 <alicelh1600@outlook.com>
2024-03-30 11:08:56 -0600

    reformat code

--- tests/natural_language_processing_tools/token_processor/token_spellchecker/test_SpellcheckerDamerauLevenshteinDistance.py
+++ tests/natural_language_processing_tools/token_processor/token_spellchecker/test_SpellcheckerDamerauLevenshteinDistance.py
@@ -14,28 +14,27 @@ class TestSpellcheckerDamerauLevensteinDistance:
     @pytest.fixture(autouse=True)
     def load_env_variables(self):
         load_dotenv()
+
     @pytest.fixture
     def spellchecker(self, mocker):
         dictionary = Dictionary()
         mocker.patch.object(
             dictionary,
             'connect_to_database',
-            return_value=True
-        )
-        damerauLevenshtein = DamerauLevenshteinDistance()
-        return SpellcheckerDamerauLevensteinDistance(dictionary, damerauLevenshtein)
+            return_value=True,
 
+        )
+        damerau_levenshtein = DamerauLevenshteinDistance()
+        return SpellcheckerDamerauLevensteinDistance(dictionary, damerau_levenshtein)
 
     @pytest.mark.SpellcheckerDamerauLevensteinDistance
     def test_search_space_for_token(self, spellchecker):
         # Given
-        token="princesa"
-        expected_search_space=['p', 'q', 'b', 'd', 'o', 'l', 'ñ', 'á', 'é', 'í', 'ó', 'ú']
+        token = "princesa"
+        expected_search_space = ['p', 'q', 'b', 'd', 'o', 'l', 'ñ', 'á', 'é', 'í', 'ó', 'ú']
         # When
         result = spellchecker.get_search_space_for_token(token)
         assert result == expected_search_space
-
-
 
     @pytest.mark.SpellcheckerDamerauLevensteinDistance
     def test_get_first_letter_of_token(self, spellchecker):
