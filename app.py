05f2babf29bfeaf8403a93f0763f61a1508bccc7
Mdperez19 <mdperezpriego@gmail.com>
2024-05-09 00:48:44 -0600

    Added front urls cors

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ from spellchecker.spellchecker_controller import spellchecker_api
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=['http://localhost:4200', 'https://sina-tt-api.azurewebsites.net/'])
+CORS(app, origins=[
+    'http://localhost:4200',
+    'https://paradis-bnkur1wx4-aliss55s-projects.vercel.app/',
+    'https://paradis-r9co4bjsb-aliss55s-projects.vercel.app/'
+])
 app.register_blueprint(spellchecker_api, url_prefix='/spellchecker')
 
 if __name__ == '__main__':
