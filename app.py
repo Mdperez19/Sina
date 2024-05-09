57840878484c32acf5d79ea872a17a7cccdbf405
Mdperez19 <mdperezpriego@gmail.com>
2024-05-09 00:59:22 -0600

    Updated url cors

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app, origins=[
     'http://localhost:4200',
-    'https://paradis-bnkur1wx4-aliss55s-projects.vercel.app/',
-    'https://paradis-r9co4bjsb-aliss55s-projects.vercel.app/'
+    'https://paradis-two.vercel.app/home',
+    r'https://paradis-.*-aliss55s-projects\.vercel\.app/',
 ])
 app.register_blueprint(spellchecker_api, url_prefix='/spellchecker')
 
