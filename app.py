e24fc0a70735912c727b601d02dde869c98f046e
Mdperez19 <mdperezpriego@gmail.com>
2024-05-09 18:06:06 -0600

    Updated url cors

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ from spellchecker.spellchecker_controller import spellchecker_api
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app, origins=[
-    'http://localhost:4200',
-    'https://paradis-two.vercel.app/home',
-    r'https://paradis-.*-aliss55s-projects\.vercel\.app/',
-])
+origins = [
+    r"https://paradis-.*\.vercel\.app",  # Vercel subdomain
+    r"http://localhost:4200"              # Localhost for development
+]
+
+# Enable CORS for all resources on this server
+cors = CORS(app, resources={r"/*": {"origins": origins}})
+
 app.register_blueprint(spellchecker_api, url_prefix='/spellchecker')
 
 if __name__ == '__main__':
